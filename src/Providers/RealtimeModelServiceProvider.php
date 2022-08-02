711b32350a0c332245711706e999d771c84ab804
Solomon Appier-Sign <braasig@gmail.com>
2022-08-02 06:34:16 +0000

    Update RealtimeModelServiceProvider.php

--- src/Providers/RealtimeModelServiceProvider.php
+++ src/Providers/RealtimeModelServiceProvider.php
@@ -7,11 +7,6 @@ use Illuminate\Support\ServiceProvider;
 
 class RealtimeModelServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        AboutCommand::add('Realtime Model', 'Version', '1.0.0');
-    }
-
     public function boot()
     {
         if ($this->app->runningInConsole()) {
